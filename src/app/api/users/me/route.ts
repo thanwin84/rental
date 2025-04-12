db76fc4fcd74dc6021b48eb342b2f59f3516f348
thanwin84 <thanwinhline84@gmail.com>
2025-04-12 23:44:06 +0600

    Update get current user information route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,19 +1,21 @@
-import { verifySession } from '@/lib/session';
+import { token } from '@/lib/constants';
+import { verifyAccessToken } from '@/lib/session';
 import User from '@/models/user.model';
 import { apiResponse } from '@/utils/apiResponse';
 import { statusCodes } from '@/utils/statusCodes';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(_request: NextRequest) {
-  const decodedToken = await verifySession();
-  if (!decodedToken.isAuthenticated) {
+export async function GET(request: NextRequest) {
+  const accessToken = request.cookies.get(token.ACCESS_TOKEN)?.value;
+  const decodedToken = await verifyAccessToken(accessToken);
+  if (decodedToken && !decodedToken.userId) {
     return NextResponse.json(
       apiResponse({ success: false, message: 'authentication invalid' }),
       { status: statusCodes.UNAUTHORIZED }
     );
   }
 
-  const user = await User.findById(decodedToken.userId);
+  const user = await User.findById(decodedToken?.userId);
   if (!user) {
     return NextResponse.json(
       apiResponse({ success: false, message: 'User does not exists' }),
