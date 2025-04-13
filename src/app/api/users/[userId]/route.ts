c6a34efb9d9e2d42f414789dba3d7c3c5220ca35
thanwin84 <thanwinhline84@gmail.com>
2025-04-13 23:50:20 +0600

    Add user update route

---
+++ src/app/api/users/[userId]/route.ts
@@ -0,0 +1,34 @@
+import { connectDb } from '@/db_connect/dbConnect';
+import User from '@/models/user.model';
+import { apiResponse } from '@/utils/apiResponse';
+import { NextRequest, NextResponse } from 'next/server';
+
+connectDb();
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { userId: string } }
+) {
+  const { userId } = params;
+  const reqBody = await req.json();
+  if (!userId) {
+    return NextResponse.json(
+      apiResponse({
+        success: false,
+        message: 'User id is missing',
+      }),
+      { status: 400 }
+    );
+  }
+  const user = await User.findByIdAndUpdate(
+    userId,
+    { $set: reqBody },
+    { new: true, runValidators: true }
+  );
+  return NextResponse.json(
+    apiResponse({
+      success: true,
+      data: user,
+      message: 'Updated successfully',
+    })
+  );
+}
