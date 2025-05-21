00fc7b5e4e1ea3cc462638329b5ddb35ffca2aa4
thanwin84 <thanwinhline84@gmail.com>
2025-05-21 17:30:37 +0600

    Enhance token retrieval by adding support for Authorization headers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,22 @@
-import { cookies } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 import { token } from './constants';
 import { verifyAccessToken } from './session';
 import { NextRequest } from 'next/server';
 
 export const getUserFromToken = async (request?: NextRequest) => {
   const cookieStore = await cookies();
-  const accessToken =
+  let accessToken =
     cookieStore.get(token.ACCESS_TOKEN)?.value ||
     request?.cookies.get(token.ACCESS_TOKEN)?.value;
+
+  if (!accessToken) {
+    const headerStore = await headers();
+    const authHeader =
+      headerStore.get('Authorization') || request?.headers.get('Authorization');
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      accessToken = authHeader.split(' ')[1];
+    }
+  }
   const decodedAccessToken = await verifyAccessToken(accessToken);
   return decodedAccessToken;
 };
