359b37cca0f30255da21614c49d97133edb5fc95
thanwin84 <thanwinhline84@gmail.com>
2025-05-27 11:25:19 +0600

    Update home page search bar

--- src/components/home/SearchBar.tsx
+++ src/components/home/SearchBar.tsx
@@ -1,16 +1,57 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { usePropertyStore } from '@/lib/store';
+import { Search } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 
 export default function SearchBar({ className }: { className?: string }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { setFilter } = usePropertyStore();
+  const [localLocation, setLocalLocation] = useState('');
+
+  function handleChange(value: string) {
+    setLocalLocation(value);
+    setFilter('location', value);
+  }
+
+  function handleSearch() {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (localLocation) {
+      const [city, country] = localLocation.split(',');
+      if (city && country) {
+        params.set('city', city.trim());
+        params.set('country', country.trim());
+      }
+      params.delete('polygon');
+      setFilter('polygon', '');
+    } else {
+      params.delete('city');
+      params.delete('country');
+    }
+
+    router.push(`/properties/search?${params.toString()}`);
+  }
+
   return (
-    <div className={`flex border rounded-md bg-white p-1  ${className}`}>
+    <div className={`flex border rounded-md bg-white ${className}`}>
       <Input
-        placeholder="Search Property"
+        placeholder='e.g. Dhaka, Bangladesh'
         style={{ outline: 'none', boxShadow: 'none' }}
-        className="border-none "
+        className='border-none'
+        value={localLocation}
+        onChange={(e) => handleChange(e.target.value)}
+        onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
       />
-      <Button className="bg-red-400 hover:bg-red-500 cursor-pointer">
-        Search
+      <Button
+        onClick={handleSearch}
+        className='bg-white hover:bg-white cursor-pointer border'
+      >
+        <Search className='text-slate-800' size={14} />
       </Button>
     </div>
   );
